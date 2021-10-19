0cc2ad381b1f18f420f525ff824cb83eed876af6
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-10-19 18:22:59 +0200

    computer now can fire one more shot if the previous was a hit.

--- src/attack-enemy.js
+++ src/attack-enemy.js
@@ -1,29 +1,9 @@
-/*
-const attackComputer = (you, computer) => {
-    const allComputerDivs = document.querySelectorAll('.computer-board > .cell');
-    allComputerDivs.forEach(div => {
-        div.addEventListener('click', e => {
-            const x = parseInt(e.target.getAttribute('x'));
-            const y = parseInt(e.target.getAttribute('y'));
-            const result = you.turn(computer, x, y);
-            if (result == 'hit') {
-                e.target.classList.add('hit');
-            } else {
-                e.target.classList.add('miss');
-            }
-            if (result == 'miss') {
-                attackPlayer(computer, you);
-            }
-            else if (result == 'hit' && computer.gameboard.allSunk()) {
-                console.log('done!');
-            }
-        })
-    })
-}
-*/
 
 const attackComputer = (you, computer) => {
     const allComputerDivs = document.querySelectorAll('.computer-board > .cell');
+    allComputerDivs.forEach(div => {
+        div.addEventListener('click', attack);
+    })
     function attack (event) {
         const x = parseInt(event.target.getAttribute('x'));
             const y = parseInt(event.target.getAttribute('y'));
@@ -36,14 +16,15 @@ const attackComputer = (you, computer) => {
             if (result == 'miss') {
                 attackPlayer(computer, you);
             }
-            else if (result == 'hit' && computer.gameboard.allSunk()) {
+            else if (result == 'hit' && (computer.gameboard.allSunk() || you.gameboard.allSunk())) {
                 console.log('done!');
+                allComputerDivs.forEach(div => {
+                    div.removeEventListener('click', attack);
+                })
             }
             event.target.removeEventListener('click', attack);
     }
-    allComputerDivs.forEach(div => {
-        div.addEventListener('click', attack);
-    })
+
 }
 
 
@@ -51,13 +32,14 @@ const attackComputer = (you, computer) => {
 
 const attackPlayer = (computer, player) => {
     const allPlayerCells = Array.from(document.querySelectorAll('.player-board > .cell'));
-    const attackResult = computer.turn(player);
+    let attackResult = computer.turn(player);
     const toFind = (element) => element.attributes[1].value == attackResult[1] && element.attributes[2].value == attackResult[2];
     const found = allPlayerCells.find(toFind);
     if (attackResult[0] == 'miss') {
         found.classList.add('miss')
     } else if (attackResult[0] == 'hit') {
-        found.classList.add('hit')
+        found.classList.add('hit');
+        attackPlayer(computer, player);
     }
     return attackResult[0];
 } 
