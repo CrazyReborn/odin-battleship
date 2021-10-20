5dc15e5d1ec5c68235692d78b3a1e0c8dab1a513
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-10-20 16:42:35 +0200

    computer can how place 4 small ships but it still can place one on top of another.

--- src/computer.js
+++ src/computer.js
@@ -18,9 +18,32 @@ const computer = () => {
         return hits;
         
     }
+
+    const randomisePlacement = () => {
+        let x;
+        let y;
+        do{
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * 10);
+        } while (gameboard.ships.some((ship) => ship.locationsH.includes(x) && ship.locationsV.includes(y)));
+        return [x, y]
+    }
+
+    const placeSmallShips = () => {
+        let i = 0;
+        do {
+            const coords = randomisePlacement();
+            gameboard.place(1, coords[0], coords[1]);
+            i++; 
+        } while(i<4)
+
+    }
+
     return {
         turn,
-        gameboard
+        gameboard,
+        randomisePlacement,
+        placeSmallShips
     }
 }
 
