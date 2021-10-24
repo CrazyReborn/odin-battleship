552f6dfbc96a0fd48eb0d73af400f6923045d17b
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-10-24 15:55:45 +0200

    fixed a bug that prevented the game to start after all the ships have been placed.

--- src/placing.js
+++ src/placing.js
@@ -1,39 +1,6 @@
 import { renderPlayerShips } from "./render-ships";
 import { attackComputer } from "./attack-enemy";
 
-// const placing = (playerOne, playerTwo) => {
-//     let count = 1;
-//     let smallShips = 0;
-//     let length = 1;
-//     ///take arragment, and length and add event listeners for player's board cells
-
-//     function place(event) {
-//         //simple version that just allows us to place ships in order from verysmall  to large
-//         const x = parseInt(event.target.getAttribute('x'));
-//         const y = parseInt(event.target.getAttribute('y'));
-//         const result = playerOne.gameboard.place(length, x, y);
-//         renderPlayerShips(playerOne);
-//         count++;
-//         if (count == 5) length = 2;
-//         else if (count == 8) length = 3;
-//         else if (count == 10) length = 4;
-//         else if (count == 11) {
-//             const allPlayerCells = document.querySelectorAll('.player-board > .cell');
-//             allPlayerCells.forEach(cell => {
-//                 cell.removeEventListener('click', place);
-//             })
-//             attackComputer(playerOne, playerTwo);
-//         }
-//         event.target.removeEventListener('click', place);
-//     }
-
-//     const allPlayerCells = document.querySelectorAll('.player-board > .cell');
-//     allPlayerCells.forEach(cell => {
-//         cell.addEventListener('click', place);
-//     })
-// }
-
-
 const placing = (playerOne, playerTwo) => {
     let lengthRadio = document.querySelectorAll('input[name="length"]');
     let length = 1;
@@ -82,16 +49,20 @@ const placing = (playerOne, playerTwo) => {
             const result = playerOne.gameboard.place(length, x, y);
             if (result == 'placed') {
                 largeShips++;
-                allPlayerCells.forEach(cell => {
-                    cell.removeEventListener('click', place);
-                })
-                attackComputer(playerOne, playerTwo);
             }
             else {
                 return;
             }
         }
+        if (verySmallShips == 4 && smallShips == 3 &&
+            mediumShips == 2 && largeShips == 1) {
+            allPlayerCells.forEach(cell => {
+                cell.removeEventListener('click', place);
+            })
+            attackComputer(playerOne, playerTwo);
+        }
         renderPlayerShips(playerOne);
+        console.log('vsmall' + verySmallShips + ' small' + smallShips + ' med ' + mediumShips + ' l ' + largeShips )
         event.target.removeEventListener('click', place);
     }
 
