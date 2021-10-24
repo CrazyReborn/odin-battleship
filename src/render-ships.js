db4ed54a1effe258e9b1535e734b8cfa52f51bd7
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2021-10-24 22:17:02 +0200

    change the rendercomputerships function to not show computer's ships.

--- src/render-ships.js
+++ src/render-ships.js
@@ -24,7 +24,7 @@ function renderComputerShips (computer) {
         const foundY = element.locationsV;
         const found = (element) => foundX.includes(parseInt(element.attributes[1].value)) && foundY.includes(parseInt(element.attributes[2].value));
         const filteredCells = allComputerCells.filter(found);
-        filteredCells.forEach(div => div.classList.add('occupied'));
+      //  filteredCells.forEach(div => div.classList.add('occupied'));      UNCOMMENT TO SEE COMPUTER's SHIPS
         if (element.newShip.isSunk()) {
             filteredCells.forEach(div => div.classList.add('down'));
         }
